ab3c375637d15eb641ce6314c6fcf010127a02e3
tifunnyy <funnyrnd92@gmail.com>
2025-03-16 20:12:20 +0700

    menambah class halogeneric dengan method sapaUser

--- tpmod5_103022330036/Program.cs
+++ tpmod5_103022330036/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-aksnain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tp5_103022330036;
+
+namespace tp5_103022330036
+{
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine($"Halo, user{x}");
+        }
+    }
+}
+
+
+﻿// See https://aka.ms/new-console-template for more information
+
+class program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric hai = new HaloGeneric();
+
+        hai.SapaUser<string>(" Praktikan");
+    }
+}
